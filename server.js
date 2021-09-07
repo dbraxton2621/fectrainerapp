9fd3e212d18e1e4f8123890ee4185925e5ace09d
Thuan Dang <thuandang@Thuans-Mac-mini.local>
2021-09-07 16:24:36 -0500

    server works

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const pool = new Pool ({
     port: 5432,
 })
 
-
 app.use(express.json())
 app.use(cors())
 
+/**********************************************************/
 //GET ALL
-app.get('/api/trainer', async (req, res) => {
+app.get('/api/trainer', async (req, res, next) => {
     try {
         const {rows} = await pool.query('SELECT * FROM trainers')
         res.status(200).json(rows)
@@ -28,7 +28,7 @@ app.get('/api/trainer', async (req, res) => {
 })
 
 // GET ONE ITEM
-app.get('/api/trainer/:id', async (req, res) => {
+app.get('/api/trainer/:id', async (req, res, next) => {
     const { id } = req.params;
     try {
         const {rows} = await pool.query('SELECT * FROM trainers WHERE trainer_id = $1', [id]);
@@ -39,7 +39,7 @@ app.get('/api/trainer/:id', async (req, res) => {
     }
 })
 
-app.post('/api/trainer/', async (req, res) => {
+app.post('/api/trainer/', async (req, res, next) => {
     try {
         const {picture, first_name, last_name, email, phone_number, bodybuilding, running, power_lifting, cycling, swimming} = req.body
         // add data validation
@@ -51,7 +51,7 @@ app.post('/api/trainer/', async (req, res) => {
     }
 })
 
-app.delete('/api/trainer/:id', async (req, res) => {
+app.delete('/api/trainer/:id', async (req, res, next) => {
     try {
         const {id} = req.params
         const {rows} = await pool.query('DELETE from trainers WHERE trainer_id = $1 RETURNING *', [id])
@@ -62,8 +62,7 @@ app.delete('/api/trainer/:id', async (req, res) => {
     }
 })
 
-
-app.patch('/api/trainer/:id', async (req, res) => {
+app.patch('/api/trainer/:id', async (req, res, next) => {
     try {
         const {picture, first_name, last_name, email, phone_number, bodybuilding, running, power_lifting, cycling, swimming} = req.body
         const {id} = req.params
@@ -75,6 +74,86 @@ app.patch('/api/trainer/:id', async (req, res) => {
         res.status(500).json(error)
     }
 })
+
+
+/**********************************************************/
+
+// GET all 
+app.get('/api/comments', async(req, res, next) => {
+    try {
+        let {rows} = await pool.query('SELECT * FROM comments')
+        res.status(200).json(rows)
+    } catch (error) {
+        console.log('Internal server error')
+        res.status(500).json(error)
+    }
+})
+
+// GET one based on user id 
+app.get('/api/comments/:id', async(req, res, next) => {
+    try {
+        const {id} = req.params
+        let {rows} = await pool.query('SELECT * FROM comments WHERE comment_id = $1', [id])
+        res.status(200).json(rows)
+    } catch (error) {
+        console.log('Internal server error')
+        res.status(500).json(error)
+    }
+})
+
+// POST one 
+app.post('/api/comments', async(req, res, next) => {
+    try {
+        const {comment_body, trainer_id} = req.body
+        // data validation
+        if (typeof comment_body !== 'string' || typeof trainer_id !== 'number') {
+            res.status(404).send('Bad request')
+        } else {
+            let {rows} = await pool.query('INSERT INTO comments(comment_body, trainer_id) VALUES ($1, $2) RETURNING *', [comment_body, trainer_id])
+            res.status(201).json(rows)
+        }
+    } catch (error) {
+        console.log('Internal Server Error')
+        res.status(500).json(error)
+    }
+})
+
+// UPDATE one
+app.patch('/api/comments/:id', async(req, res, next) => {
+    try {
+        const {id} = req.params
+        const {comment_body, trainer_id} = req.body
+        if (typeof comment_body !== 'string' || typeof trainer_id !== 'number') {
+            res.status(404).send('Bad request')
+        } else {
+            let {rows} = await pool.query('UPDATE comments SET comment_body = $1, trainer_id = $2 WHERE comment_id = $3 RETURNING *', [comment_body, trainer_id, id])
+            res.status(200).json(rows)
+        }
+    } catch (error) {
+        console.log('Internal Server Error')
+        res.status(500).json(error)
+    }
+})
+
+// DELETE one 
+app.delete('/api/comments/:id', async(req, res, next) => {
+    try {
+        const {id} = req.params
+        let {rows} = await pool.query('DELETE FROM comments WHERE comment_id = $1 RETURNING *', [id])
+        res.status(200).json(rows)
+    } catch (error) {
+        console.log('Internal Server Error')
+        res.status(500).json(error)
+    }
+})
+
+/**********************************************************/
+
+// handle unknown http reqs
+app.use( (req, res, next) => {
+    res.status(404).end("Not Found")
+})
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
